d3d08dbb2149c507af35e6691d22dfbadc7e08f0
emmanuelc <camuschinox@gmail.com>
2024-01-22 17:30:03 -0300

    Added a pokemon tables and its foreign key

--- database.sql
+++ database.sql
@@ -1,11 +1,22 @@
 CREATE DATABASE IF NOT EXISTS pokemon_ico;
 
-CREATE TABLE IF NOT EXISTS pokemon_ico.pokemon_match (
+
+CREATE TABLE IF NOT EXISTS pokemon_ico.training_pokemon (
     id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    first_pokemon varchar(20) NOT NULL,
-    second_pokemon varchar(20) NOT NULL,
-    who_won varchar(6) NOT NULL,
+    name varchar(20),
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     primary key (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS pokemon_ico.pokemon_match (
+    id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    first_pokemon_id bigint(20) unsigned NOT NULL,
+    second_pokemon_id bigint(20) unsigned NOT NULL,
+    who_won varchar(6) NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    primary key (id),
+    foreign key (first_pokemon_id) references training_pokemon(id)
 );
